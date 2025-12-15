65356b4a31ea693180bba4c3d815b176dc1d062a
suriel0708 <20197706@itla.edu.do>
2025-12-14 23:27:43 -0400

    Deploy automático con GitHub Actions

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express();
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hola Mundo DevOps 🚀');
+  res.send('Hola Mundo DevOps CI/CD FINAL 🚀');
 });
 
 app.listen(PORT, () => {
